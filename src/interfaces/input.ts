927352595f2ddc91da68e98ef010ee21e9887bc8
adil-egenie <arehman.egenienext@gmail.com>
2023-07-24 18:57:32 +0500

    date range

--- src/interfaces/input.ts
+++ src/interfaces/input.ts
@@ -1,3 +1,13 @@
+import {Range} from "react-date-range"
+
+interface DefaultField {
+    className?: string,
+    label: string,
+    placeholder?: string,
+    icon?: any,
+    onChange?: (value?: any) => void,
+    name?: string
+}
 interface IInputField {
     className?: string,
     label: string,
@@ -6,11 +16,22 @@ interface IInputField {
     placeholder?: string,
     icon?: any,
     onChange?: (value?: any) => void,
+    onAdditionalChange?: (value?: any) => void,
     name?: string
 }
 
 export interface ISelectField extends IInputField  {
-    data: any
+    data: ISelectOptions[]
+}
+
+export interface ISelectOptions {
+    additional?: boolean
+    key: string | number
+    value: string
+}
+
+export interface IDateRangeField extends DefaultField  {
+    value: Range
 }
 
 export default IInputField
