5aaf271986fc39a8cc07c1e702749d32d9f85b63
adil-egenie <arehman.egenienext@gmail.com>
2023-07-24 18:57:32 +0500

    date range

--- src/components/UIComponents/InputField/DateRangeField.tsx
+++ src/components/UIComponents/InputField/DateRangeField.tsx
@@ -1,11 +1,21 @@
-import { ISelectField, ISelectOptions } from '@/interfaces/input'
+import { IDateRangeField } from '@/interfaces/input'
 import React, { useEffect, useRef, useState } from 'react'
+import { addDays } from 'date-fns';
+import { DateRange, Range } from "react-date-range"
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
-const SelectField = ({className, label, data=[], value, placeholder="", icon, onChange= (val)=>{}, onAdditionalChange = (val) => {}}: ISelectField) => {
+const DateRangeField = ({className, label, value, placeholder="", icon, onChange= (val: Range)=>{}}: IDateRangeField) => {
+
+    const [date, setDate] = useState<Range>(value)
 
     const [openDropDown, setOpenDropDown] = useState(false)
     const selectRef = useRef<HTMLDivElement | null>(null)
     const dropDownRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        onChange(date)
+    }, [date])
 
     useEffect(() => {
         if(selectRef)
@@ -44,20 +54,20 @@ const SelectField = ({className, label, data=[], value, placeholder="", icon, on
         console.log('openDropDown', openDropDown)
     }, [openDropDown])
 
-    const stringVal: string = data.find(d => d.key!==0 && d.key == value)?.value ?? ''
-
     return (
         <div ref={selectRef} className={`relative cursor-pointer select-none ${className}`}>
             <div className="relative">
                 <span className="border border-[#C9D2DD] bg-white rounded-2xl py-4 px-5 flex items-center justify-between"
                 onClick={() => setOpenDropDown(!openDropDown)}
                 >
-                    {
-                        icon && <span className="mr-1">{icon}</span>
-                    }
-                    <span className={`${value ? 'text-black' : `text-[var(--lite-gray)]`} overflow-ellipsis overflow-hidden whitespace-nowrap`}>
-                        {stringVal ? stringVal : (placeholder ? placeholder : label)}
-                    </span>
+                    <div className="flex items-center">
+                        {
+                            icon && <span className="mr-1">{icon}</span>
+                        }
+                        <span className={`${value ? 'text-black' : `text-[var(--lite-gray)]`} overflow-ellipsis overflow-hidden whitespace-nowrap`}>
+                            {value?.startDate ? `${new Date(value.startDate).toLocaleDateString()}${ value?.endDate ? '-' + new Date(value?.endDate).toLocaleDateString() : ''}` : (placeholder ? placeholder : label)}
+                        </span>
+                    </div>
                     <span className="">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -69,30 +79,20 @@ const SelectField = ({className, label, data=[], value, placeholder="", icon, on
             style={{background: "linear-gradient(360deg, #fff, #fff, #fff, transparent, transparent)"}}
             >{label}</label>
 
-            <div ref={dropDownRef} className={`hidden opacity-0 absolute top-[50px] min-w-[250px] w-full bg-white border-gray-100 rounded-xl large-shadow overflow-hidden large-shadow z-[9] transition-all duration-300`}>
-                <ul className="list-none max-h-[300px] overflow-auto">
-                    {
-                        data.map((d: ISelectOptions, i: number) => {
-                            return <li key={i} className={`px-3 py-2 cursor-pointer hover:bg-gray-50 ${value === d.key ? 'bg-[var(--dim-gray)]' : ''}`}
-                            onClick={() => {
-                                setOpenDropDown(false)
-                                if(!d.additional)
-                                {
-                                    onChange(d.key)
-                                }
-                                else
-                                {
-                                    onAdditionalChange(d)
-                                }
-                            }}
-                            >{d.value}</li>
-                        })
-                    }
-                </ul>
+            <div ref={dropDownRef} className={`hidden opacity-0 absolute top-[50px] min-w-[250px] w-auto bg-white border-gray-100 rounded-xl large-shadow overflow-hidden large-shadow z-[9] transition-all duration-300`}>
+                <DateRange 
+                    minDate={addDays(new Date(), -0)}
+                    editableDateInputs={true}
+                    moveRangeOnFirstSelection={false}
+                    ranges={[date]}
+                    onChange={(item) => {
+                        setDate(item.selection)
+                    }}
+                />
             </div>
         </div>
     )
 
 }
 
-export default SelectField
+export default DateRangeField
