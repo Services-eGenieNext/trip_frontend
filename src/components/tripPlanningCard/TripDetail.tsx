dbc304cb8d486001699fa0839b609976b466d7e8
Adil Rehman <arehman.egenienext@gmail.com>
2023-08-22 20:24:03 +0500

    detail modal issue

--- src/components/tripPlanningCard/TripDetail.tsx
+++ src/components/tripPlanningCard/TripDetail.tsx
@@ -70,21 +70,78 @@ const TripDetail = ({item}: ITripDetail) => {
     }, [item])
 
     return (
-        <div className=' sm:px-0 px-4'>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                <div className="h-[200px] bg-gray-100 rounded-xl overflow-hidden relative">
+        <div className='w-full sm:px-0 px-4'>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full">
+                <div className="h-[200px] min-w-[400px] w-full bg-gray-100 rounded-xl overflow-hidden relative">
                     {
-                        (!detailLoading && image) && <Image src={image} fill={true} alt={itemDetail?.name} style={{objectFit: "cover"}} />
+                        (!detailLoading && image) ? 
+                            <Image src={image} fill={true} alt={itemDetail?.name} style={{objectFit: "cover"}} /> : 
+                            <div className="animate-pulse flex justify-center items-center h-full">
+                            <svg
+                            className="w-20 h-20 text-gray-200 dark:text-gray-600"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 16 20"
+                            >
+                            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                            </svg>
+                        </div>
                     }
                 </div>
                 <div className="grid grid-cols-2 gap-2">
                     {
                         detailLoading || images.length == 0 ? (
                             <>
-                                <div className="h-[98px] bg-gray-100 rounded-xl"></div>
-                                <div className="h-[98px] bg-gray-100 rounded-xl"></div>
-                                <div className="h-[98px] bg-gray-100 rounded-xl"></div>
-                                <div className="h-[98px] bg-gray-100 rounded-xl"></div>
+                                <div className="h-[98px] w-[200px] bg-gray-100 rounded-xl animate-pulse flex justify-center items-center">
+                                    <svg
+                                    className="w-16 h-16 text-gray-200 dark:text-gray-600"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 20"
+                                    >
+                                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                    </svg>
+                                </div>
+                                <div className="h-[98px] w-[200px] bg-gray-100 rounded-xl animate-pulse flex justify-center items-center">
+                                    <svg
+                                    className="w-16 h-16 text-gray-200 dark:text-gray-600"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 20"
+                                    >
+                                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                    </svg>
+                                </div>
+                                <div className="h-[98px] w-[200px] bg-gray-100 rounded-xl animate-pulse flex justify-center items-center">
+                                    <svg
+                                    className="w-16 h-16 text-gray-200 dark:text-gray-600"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 20"
+                                    >
+                                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                    </svg>
+                                </div>
+                                <div className="h-[98px] w-[200px] bg-gray-100 rounded-xl animate-pulse flex justify-center items-center">
+                                    <svg
+                                    className="w-16 h-16 text-gray-200 dark:text-gray-600"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 20"
+                                    >
+                                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                    </svg>
+                                </div>
                             </>
                         ) : (
                             images.slice(0,4).map((img: string, imgIndex: number) => {
