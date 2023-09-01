b3352ee4b294dc41f0306069b092375625bd596c
Adil Rehman <arehman.egenienext@gmail.com>
2023-09-01 17:51:12 +0500

    view more pics

--- src/components/tripPlanningCard/TripDetail.tsx
+++ src/components/tripPlanningCard/TripDetail.tsx
@@ -5,6 +5,10 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import MapIcon from "/public/images/google-map-icon.png";
 import Link from 'next/link'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CSS from "./TripDetail.module.css"
 
 interface ITripDetail {
     item?: any
@@ -71,6 +75,117 @@ const TripDetail = ({item}: ITripDetail) => {
         }
         _def()
     }, [item])
+
+
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(false)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(false)
+    function SampleNextArrow(props: any) {
+        const { className, style, onClick } = props;
+        return (
+        <div
+            className={nextBtnDisabled == true ? `cursor-pointer select-none ${CSS["activity_arrow_next_disabled"]} `: `cursor-pointer select-none  ${CSS["activity_arrow_next"]}`}
+            style={{
+            ...style,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            }}
+            onClick={onClick}
+        >
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+            >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+            </svg>
+        </div>
+        );
+    }
+    
+    function SamplePrevArrow(props: any) {
+        const { className, style, onClick } = props;
+        return (
+        <div
+            className={prevBtnDisabled == true ? `cursor-pointer select-none ${CSS["activity_arrow_prev_disabled"]}` : `cursor-pointer select-none ${CSS["activity_arrow_prev"]}`}
+            style={{
+            ...style,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            }}
+            onClick={onClick}
+        >
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+            >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+            </svg>
+        </div>
+        );
+    }
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: false,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />,
+            },
+        },
+        {
+            breakpoint: 800,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: false,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />,
+            },
+        },
+        {
+            breakpoint: 550,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: false,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />,
+            },
+        },
+        ],
+        // afterChange,
+    };
 
     return (
         <div className='w-full sm:px-0 px-4'>
@@ -160,13 +275,20 @@ const TripDetail = ({item}: ITripDetail) => {
             {
                 images.length > 4 && <span className="text-[var(--blue)] text-right ml-auto block cursor-pointer select-none" onClick={() => setViewMore(!viewMore)}>View more pics</span>
             }
-            <div className="flex flex-wrap">
+            <div className="w-[90%] mx-auto arrow_remove mt-4">
                 {
-                    viewMore && images.slice(4).map((img: string, imgIndex: number) => {
-                        return <div key={imgIndex} className="h-[98px] w-[200px] bg-gray-100 rounded-xl overflow-hidden relative mx-1 mb-2">
-                            <Image src={img} fill={true} alt={itemDetail?.name} style={{objectFit: "cover"}} />
-                        </div>
-                    })
+                    viewMore && 
+                    <Slider {...settings}>
+                        {
+                            images.slice(4).map((img: string, imgIndex: number) => {
+                                return <div className="px-1">
+                                    <div key={imgIndex} className="h-[98px] w-[100%] bg-gray-100 rounded-xl overflow-hidden relative">
+                                        <Image src={img} fill={true} alt={itemDetail?.name} style={{objectFit: "cover"}} />
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </Slider>
                 }
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
