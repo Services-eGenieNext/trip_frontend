468d8c4ffb472704da2860440c9a8d604a5892a7
Adil Rehman <arehman.egenienext@gmail.com>
2023-09-07 17:49:12 +0500

    sorting in multi select

--- src/components/UIComponents/MultiSelectDropdown/index.tsx
+++ src/components/UIComponents/MultiSelectDropdown/index.tsx
@@ -26,6 +26,7 @@ interface TypeProps {
   disabled?:any;
   saveData?:any;
   setSaveData?:any;
+  allowSorting?: boolean
 }
 
 interface TypeOpt {
@@ -56,6 +57,7 @@ export default function SelectCheckBoxSimple({
   className,
   saveData,
   setSaveData,
+  allowSorting
 }: TypeProps) {
   const dispatch = useAppDispatch();
   const [showDropDown, setShowDropDown] = useState(false);
@@ -450,13 +452,17 @@ if(addCustomeOption == false){
                 })
                 }
               </div>
-              <div className="text-[var(--blue)]">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.4} stroke="currentColor" className="w-7 h-7 p-[3px] mb-[5px] cursor-pointer"
-                onClick={() => setShowSorting(true)}
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
-                </svg>
-              </div>
+              {
+                allowSorting && optsSelected.length > 1 && (
+                  <div className="text-[var(--blue)]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.4} stroke="currentColor" className="w-7 h-7 p-[3px] mb-[5px] cursor-pointer"
+                    onClick={() => setShowSorting(true)}
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                    </svg>
+                  </div>
+                )
+              }
             </div>
           )}
 
