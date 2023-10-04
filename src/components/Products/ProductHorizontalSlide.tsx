771677f68cd42481960df375d6d1b19337fbead1
Zain Saleem <zain.egenie@gmail.com>
2023-10-04 20:41:53 +0500

    resolve sliders issue and load more functionality in modal reviews

--- src/components/Products/ProductHorizontalSlide.tsx
+++ src/components/Products/ProductHorizontalSlide.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from "react";
 import Section from "../UIComponents/Section";
 import ComponentTitle from "../UIComponents/ComponentTitle";
 import SliderComponent from "../UIComponents/Sliders/Slider";
+import ModalSliderComponent from "../UIComponents/Sliders/modalSlider";
 import { BlankStar, FilledStar } from "../icons/Stars";
 import InputField from "../UIComponents/InputField/InputField";
 import TimerOutlined from "../icons/TimerOutlined";
@@ -47,10 +48,15 @@ const ProductHorizontalSlide = ({
   const { itineraryDays } = useAppSelector(state => state.itineraryReducer)
 
   useEffect(() => {
-    setLocations(locationsState);
+    if(locationsState.length > 0){
+      setLocations(locationsState);
+    }else{
+      setLocations([])
+    }
   }, [locationsState]);
 
   useEffect(() => {
+    console.log(locations,"locations")
     if (locations.length > 0) {
       setLoading(false);
     }else{
@@ -157,7 +163,8 @@ const ProductHorizontalSlide = ({
         </div>
         )}
       <div ref={slideRef} id="location-to-visit-slide" className="mt-10">
-        <SliderComponent slidesToShow={slidesToShow} route={route} >
+        {route == "Trending Location" ? (
+          <ModalSliderComponent>
           {loading === true
             ? skelton.map((limit: string, index: number) => {
                 return (
@@ -183,7 +190,173 @@ const ProductHorizontalSlide = ({
                   </div>
                 );
               })
-            : locations?.map((location: any, index) => {
+            : locations?.map((location: any, index:number) => {
+                let image_path =
+                  location.images === "" ? BlankLocation.src : location.images;
+                let address = location.formatted_address
+                  ? location.formatted_address
+                  : location.address_obj?.address_string;
+
+                let isExistLocArr: any[] = itineraryDays.map(itinerary => itinerary.times.map(time => time.location.name === location.name))
+                let locArrBoolean: boolean[] = [].concat(...isExistLocArr)
+                let isExistInItinerary = locArrBoolean.includes(true)
+
+                return (
+                    <div key={index} className={`px-2 md:max-w-[300px] h-[310px] w-full md:mt-0 mt-10 ${v_type == "3" && "cursor-move" }`} >
+                      <Tooltip title={(v_type === "3") ? "Dragable in itinerary days." : ""}>
+                      <div className={`grid grid-cols-1 ${v_type == "3" ? 'rounded-3xl shadow-lg h-max' : 'rounded-xl shadow-sm h-full'} border overflow-hidden relative ${styles["slider_card"]}`} draggable={(v_type === "3") ? true : false} onDragStart={(e) => dragStartFunc(e, location)} >
+                        <div className={`${ type == "title-card" ? 'h-[310px]' : 'h-[178px]'} bg-gray-100 relative`} >
+                         {route !== "Trending Location" && (
+                           <div className={`absolute top-0 left-0 z-[1] ${styles.tag} ${isExistInItinerary ? styles.blue : styles.green}`}> {isExistInItinerary ? 'In' : 'Out'} </div>
+                         )}
+                          <Image
+                            src={image_path}
+                            alt={location.name}
+                            fill={true}
+                            style={{ objectFit: "cover" }}
+                          />
+                          <div
+                            className="absolute inset-0"
+                            style={{
+                              background:
+                                "linear-gradient(0deg, rgb(0 0 0 / 70%), transparent)",
+                            }}
+                          ></div>
+                          {
+                            (isHover == true) && (
+                              <div
+                                className={`absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center ${styles["hover_overlay"]}`}
+                              >
+                                {
+                                  isAutomate && (
+                                    <Link
+                                      href={`/trip-plan?address=${address}&location_id=${location.location_id ?? ''}&place_id=${location.place_id ?? ''}&v_type=${v_type}`}
+                                      className="h-[40px] rounded-md bg-[#009DE2] text-white hover:bg-transparent border hover:border-[#009DE2] hover:text-white w-[170px] flex justify-center items-center"
+                                    >
+                                      Automate My Trip
+                                    </Link>
+                                  )
+                                }
+                                <button
+                                  className="h-[40px] rounded-md text-white border border-white mt-5 w-[170px] hover:bg-[#009DE2]"
+                                  onClick={() => {
+                                    console.log("location", location);
+                                    dispatch(setItem({
+                                      locaiton_id: location.location_id,
+                                      place_id: location.place_id,
+                                    }));
+                                  }}
+                                >
+                                  More Info
+                                </button>
+                              </div>
+                            )
+                          }
+                        </div>
+                        <div className={`${type == "title-card" ? "absolute bottom-4 left-4 text-white font-bold text-[25px] pe-5" : "p-4"}`}>
+                          <div className="grid grid-cols-2 justify-center items-center mb-2 relative px-5">
+                            <div>
+                              <h4 className={`overflow-hidden overflow-ellipsis whitespace-nowrap ${isAddButton ? "col-span-1" : "col-span-2"} `} >
+                                {location.name}
+                              </h4>
+                            </div>
+                            
+                            {isAddButton && (
+                              <div
+                                className="flex justify-end items-center gap-2 cursor-pointer"
+                                onClick={(e) => {
+                                  setOpenLocation(location)
+                                  placeForm(e)
+                                }}
+                              >
+                                <span className="text-[11px] text-[var(--green)]">
+                                  Add
+                                </span>
+                                <span className="w-[23px] h-[23px] rounded-full bg-[var(--lite-green)] hover:bg-[var(--green)] text-[var(--green)] hover:text-white flex justify-center items-center transition-all duration-300">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={2}
+                                    stroke="currentColor"
+                                    className="w-[15px] h-[15px]"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M12 4.5v15m7.5-7.5h-15"
+                                    />
+                                  </svg>
+                                </span>
+                              </div>
+                            )}
+                          </div>
+                          {
+                            url == "variation_2" && (
+                              <div className="flex flex-wrap justify-center gap-2 items-center my-2">
+                                <span>{location?.rating ? location?.rating : ""}</span>
+                                {
+                                  reviewArr &&
+                                  reviewArr.map((review, index) => {
+                                    if (index < location.rating) {
+                                      return <FilledStar key={index} />;
+                                    } else {
+                                      return <BlankStar key={index} />;
+                                    }
+                                  })
+                                }
+                                <span className="text-[var(--lite-gray)]">
+                                  {location.user_ratings_total ? `(${location.user_ratings_total})` : ""}
+                                </span>
+                              </div>
+                            )
+                          }
+                          {url == "variation_3" && (
+                            <div className="flex gap-2 items-center absolute top-[7px] right-[7px] bg-white px-3 border rounded-lg">
+                              <span>{location?.rating}</span>
+                              <FilledStar />
+                            </div>
+                          )}
+                          {isDesc && (
+                            <p className="text-[15px] text-[var(--gray)]">
+                              
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      </Tooltip>
+                    </div>
+                );
+              })}
+        </ModalSliderComponent>
+        ):(
+        <SliderComponent slidesToShow={slidesToShow} >
+          {loading === true
+            ? skelton.map((limit: string, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    role="status"
+                    className="max-w-sm rounded animate-pulse h-[350px] md:max-w-[300px] relative px-3 md:mt-0 mt-10"
+                  >
+                    <div className="flex items-center justify-center mb-4 bg-gray-300 rounded dark:bg-gray-700 h-[350px]">
+                      <svg
+                        className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 16 20"
+                      >
+                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                      </svg>
+                    </div>
+                    <div className="h-[40px] bg-gray-200 rounded-md dark:bg-gray-700 w-[50%] mb-4 absolute bottom-4 left-6 z-10"></div>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                );
+              })
+            : locations?.map((location: any, index:number) => {
                 let image_path =
                   location.images === "" ? BlankLocation.src : location.images;
                 let address = location.formatted_address
@@ -322,6 +495,7 @@ const ProductHorizontalSlide = ({
                 );
               })}
         </SliderComponent>
+        )}
       </div>
 
       <div
