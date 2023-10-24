d55129ca6b28023377b837a8a8b92f195adaa840
Adil Rehman <arehman.egenienext@gmail.com>
2023-10-24 21:18:52 +0500

    record updated

--- src/components/tripPlanningCard/pricing-cards/functions.ts
+++ src/components/tripPlanningCard/pricing-cards/functions.ts
@@ -26,81 +26,91 @@ const getTime = async (time: string) => {
 }
 
 const calculateDuration = async (times: any, i: number, duration: string) => {
-    let _time = times[i].time.replace(' – ', ' - ')
-    if(_time.search(' - ') !== -1)
-    {
+    // let _time = times[i].time.replace(' – ', ' - ')
+    let _currentStartTime;
+    let _ActualEndTime;
+    let _currentEndTime;
 
-        let _currentStartTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[0]
+    if(times[i].time.search('Open') == -1) // if limited time not Open for 24 hours
+    {
+        _currentStartTime = times[i].time.replace(' – ', ' - ').split(' - ')[0]
         _currentStartTime = await getTime(_currentStartTime)
 
-        let _ActualEndTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[1]
+        _ActualEndTime = times[i].time.replace(' – ', ' - ').split(' - ')[1]
         _ActualEndTime = await getTime(_ActualEndTime)
 
-        let _currentEndTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[0]
+        _currentEndTime = times[i].time.replace(' – ', ' - ').split(' - ')[0]
         _currentEndTime = await getTime(_currentEndTime)
         _currentEndTime.setHours(_currentEndTime.getHours() + 2)
-
-        let _prevEndTime: any = times[i-1].suggestedTime?.endTime
-
-
-        if(!_prevEndTime)
-        {
-            return {
-                startTime: `${_currentStartTime.getHours()}:${_currentStartTime.getMinutes()}`,
-                endTime: `${_currentEndTime.getHours()}:${_currentEndTime.getMinutes()}`,
-            }
-        }
-
-        _prevEndTime = await getTime(_prevEndTime)
-
-        if(duration.search('days') !== -1)
-        {
-            _prevEndTime.setDate(_prevEndTime.getDate() + Number(duration.substring(0,2)))
-        }
-        else if(duration.search('hours') !== -1)
-        {
-            _prevEndTime.setHours(_prevEndTime.getHours() + Number(duration.substring(0,2)))
-        }
-        else if(duration.search('min') !== -1)
-        {
-            _prevEndTime.setMinutes(_prevEndTime.getMinutes() + Number(duration.substring(0,2)))
-        }
-
-        let suggestedStartTime = _currentStartTime
-        let blockedStartTime = await getTime('07:00 AM')
-        let blockedEndTime = await getTime('10:00 PM')
-        if(_prevEndTime.getTime() >= _currentStartTime.getTime() && _prevEndTime.getTime() < _ActualEndTime.getTime())
-        {
-            suggestedStartTime = _prevEndTime
-        }
-        let startTime = `${suggestedStartTime.getHours()}:${suggestedStartTime.getMinutes()}`
-        
-
-        // set end time as after 2 hours of start time
-        let suggestedLastTime: any = suggestedStartTime
-        suggestedLastTime.setHours(suggestedLastTime.getHours() + 2)
-
-        if(suggestedLastTime.getTime() < suggestedStartTime.getTime() || suggestedLastTime.getTime() > _ActualEndTime.getTime())
-        {
-            suggestedLastTime = _ActualEndTime
-        }
-
-        if(suggestedLastTime.getTime() > blockedEndTime.getTime() || suggestedLastTime.getTime() < blockedStartTime.getTime())
-        {
-            suggestedLastTime = blockedEndTime
-        }
-
-        let endTime = `${suggestedLastTime.getHours()}:${suggestedLastTime.getMinutes()}`
-
-        return {
-            startTime: startTime,
-            endTime: endTime,
-        }
-
     }
+    else // if time Open for 24 hours
+    {
+        _currentStartTime = times[i-1].suggestedTime?.endTime
+        _currentStartTime = await getTime(_currentStartTime)
+
+        console.log('_currentStartTime', _currentStartTime)
+
+        _ActualEndTime = new Date(_currentStartTime)
+        _ActualEndTime.setHours(_ActualEndTime.getHours() + 2)
+        console.log('_ActualEndTime', _ActualEndTime)
+
+        _currentEndTime = new Date(_ActualEndTime)
+        console.log('_currentEndTime', _currentEndTime)
+    }
+
+    let _prevEndTime: any = times[i-1].suggestedTime?.endTime
+
+    if(!_prevEndTime)
+    {
+        return {
+            startTime: `${_currentStartTime.getHours()}:${_currentStartTime.getMinutes()}`,
+            endTime: `${_currentEndTime.getHours()}:${_currentEndTime.getMinutes()}`,
+        }
+    }
+
+    _prevEndTime = await getTime(_prevEndTime)
+
+    if(duration.search('days') !== -1)
+    {
+        _prevEndTime.setDate(_prevEndTime.getDate() + Number(duration.substring(0,2)))
+    }
+    else if(duration.search('hours') !== -1)
+    {
+        _prevEndTime.setHours(_prevEndTime.getHours() + Number(duration.substring(0,2)))
+    }
+    else if(duration.search('min') !== -1)
+    {
+        _prevEndTime.setMinutes(_prevEndTime.getMinutes() + Number(duration.substring(0,2)))
+    }
+
+    let suggestedStartTime = _currentStartTime
+    let blockedStartTime = await getTime('07:00 AM')
+    let blockedEndTime = await getTime('10:00 PM')
+    if(_prevEndTime.getTime() >= _currentStartTime.getTime() && _prevEndTime.getTime() < _ActualEndTime.getTime())
+    {
+        suggestedStartTime = _prevEndTime
+    }
+    let startTime = `${suggestedStartTime.getHours()}:${suggestedStartTime.getMinutes()}`    
+
+    // set end time as after 2 hours of start time
+    let suggestedLastTime: any = suggestedStartTime
+    suggestedLastTime.setHours(suggestedLastTime.getHours() + 2)
+
+    if(suggestedLastTime.getTime() < suggestedStartTime.getTime() || suggestedLastTime.getTime() > _ActualEndTime.getTime())
+    {
+        suggestedLastTime = _ActualEndTime
+    }
+
+    if(suggestedLastTime.getTime() > blockedEndTime.getTime() || suggestedLastTime.getTime() < blockedStartTime.getTime())
+    {
+        suggestedLastTime = blockedEndTime
+    }
+
+    let endTime = `${suggestedLastTime.getHours()}:${suggestedLastTime.getMinutes()}`
+
     return {
-        startTime: _time,
-        endTime: '',
+        startTime: startTime,
+        endTime: endTime,
     }
 }
 
@@ -108,24 +118,22 @@ const _calculateStartAndEndTime = async (times: any, i: number) => {
 
     let _currentStartTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[0]
     
-    // when found open for 24 hours then return same time
-    if(_currentStartTime.search('Open') !== -1)
+    // when found open for 24 hours in first location then return static time
+    if(_currentStartTime.search('Open') !== -1 && i == 0)
     {
         return {
-            startTime: `${_currentStartTime}`,
-            endTime: '',
+            startTime: '7:00',
+            endTime: '9:00',
         }
     }
 
-    _currentStartTime = await getTime(_currentStartTime)
-
-
-    let _currentEndTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[0]
-    _currentEndTime = await getTime(_currentEndTime)
-    _currentEndTime.setHours(_currentEndTime.getHours() + 2)
-
     if(i == 0)
     {
+        _currentStartTime = await getTime(_currentStartTime)
+        let _currentEndTime: any = times[i].time.replace(' – ', ' - ').split(' - ')[0]
+        _currentEndTime = await getTime(_currentEndTime)
+        _currentEndTime.setHours(_currentEndTime.getHours() + 2)
+
         return {
             startTime: `${_currentStartTime.getHours()}:${_currentStartTime.getMinutes()}`,
             endTime: `${_currentEndTime.getHours()}:${_currentEndTime.getMinutes()}`,
