d02e80bb5dc18ac6546887df15fd26ce82afe796
Zain Saleem <zain.egenie@gmail.com>
2023-10-26 18:05:22 +0500

    resolved sorting priority numbering

--- src/components/UIComponents/MultiSelectDropdown/sort-popup-options.tsx
+++ src/components/UIComponents/MultiSelectDropdown/sort-popup-options.tsx
@@ -97,7 +97,7 @@ const SortPopupOptions = ({index, opt, onSort}: ISortPopupOptions) => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 rotate-90 my-auto">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                 </svg>
-                <span className="bg-[var(--blue)] text-white text-center p-2 w-full ">{opt.opt}</span>
+                <span className="bg-[var(--blue)] text-white text-center p-2 w-full ">{index+1}{"."}{" "}{opt.opt}</span>
             </span>
 
             <div className={`w-full h-[5px] rounded-xl relative transition-all duration-300`} />
